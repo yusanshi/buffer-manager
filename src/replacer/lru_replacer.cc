2d474ec4753fe43fe9f1a9d8963e2c60e091333d
yusanshi <meet.leiyu@gmail.com>
2021-11-21 22:15:48 +0800

    Fix bug in 'GetVictim'

--- src/replacer/lru_replacer.cc
+++ src/replacer/lru_replacer.cc
@@ -7,7 +7,7 @@ int LRUReplacer::GetVictim() {
        ++i) {
     auto cache_descriptor = *i;
     if (cache_descriptor->pin_count == 0) {
-      this->cache_list_.pop_back();
+      this->cache_list_.erase(std::next(i).base());
       this->page2cache_.erase(cache_descriptor->page_id);
       return cache_descriptor->page_id;
     }
@@ -26,12 +26,12 @@ void LRUReplacer::DecreasePinCount(int page_id) {
 void LRUReplacer::PolishPage(int page_id, int frame_id) {
   LRUCacheDescriptor* cache_descriptor;
   if (this->page2cache_.find(page_id) == this->page2cache_.end()) {
-    // If not in LRU list, create
+    // If not in LRU list, create it and push front (later)
     cache_descriptor = new LRUCacheDescriptor();
     cache_descriptor->page_id = page_id;
     cache_descriptor->frame_id = frame_id;
   } else {
-    // If in the list, remove it
+    // Else, remove it and push front (later)
     auto position = this->page2cache_.at(page_id);
     this->cache_list_.erase(position);
     cache_descriptor = *position;
